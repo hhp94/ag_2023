46c2f2c62917a04b1b8217a2fa0c40fcee1c16fd
Hung Pham <58577711+hhp94@users.noreply.github.com>
2023-01-17 07:33:10 -0500

    before implementing dynamic UI

---
+++ R/dynamic_filter.R
@@ -0,0 +1,25 @@
+make_ui <- function(x, var, label_fns) {
+  UseMethod("make_ui")
+}
+
+make_ui.numeric <- function(x, var, label_fns) {
+  rng <- range(x, na.rm = TRUE)
+  sliderInput(var, label_fns(var), min = rng[1], max = rng[2], value = rng)
+}
+
+make_ui.factor <- function(x, var, label_fns) {
+  levs <- levels(x)
+  selectInput(var, label_fns(var), choices = levs, selected = levs, multiple = TRUE)
+}
+
+filter_var <- function(x, val) {
+  UseMethod("filter_var")
+}
+
+filter_var.numeric <- function(x, val) {
+  !is.na(x) & x >= val[1] & x <= val[2]
+}
+
+filter_var.factor <- function(x, val) {
+  x %in% val
+}
